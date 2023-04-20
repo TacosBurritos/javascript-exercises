728b7ace159eeca3d2ba61d9bab13513ff934a6d
Takafumi Mashiko <tmashiko7@gmail.com>
2023-04-20 11:22:14 -0400

    finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let number = phrase.length - 1;
+    let reverseWord = "";
+    for(i = number; i >= 0; i--){
+        reverseWord += phrase[i];
+    }
+    return reverseWord;
 };
 
 // Do not edit below this line
