261235d274e6289a0e7d94498d0cdb82e8e3ba38
Takafumi Mashiko <tmashiko7@gmail.com>
2023-04-19 21:00:49 -0400

    first try

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+let phrase = '';
+let i = 0;
+const repeatString = function(phrase, i) {
+    for (i; i <= 0; i--){
+        return phrase;
+    }
 };
 
 // Do not edit below this line
