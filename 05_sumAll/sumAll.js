c0b8cab36e0242d34334c1bbe925e923c688fb2f
Takafumi Mashiko <tmashiko7@gmail.com>
2023-04-20 19:07:13 -0400

    finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,20 @@
 const sumAll = function(start, end) {
     let sum = 0;
-    for(start; start < end; start++){
-        sum += start;
+    if(!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0){
+        return 'ERROR'
     }
-    return sum;
+    else{
+        if(start > end){
+            let temp = start;
+            start = end;
+            end = temp;
+        }
+        for(start; start <= end; start++){
+            sum += start;
+        }
+        return sum;
+    }
+    
 };
 
 // Do not edit below this line
