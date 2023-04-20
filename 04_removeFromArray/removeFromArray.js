d14cd2c532b49b85dfaa5c1ee8c44bb59e9ee06a
Takafumi Mashiko <tmashiko7@gmail.com>
2023-04-20 18:27:58 -0400

    finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
 
+const removeFromArray = function(testArray, ...removeElem) {
+   for (let i = 0; i < removeElem.length; i++){
+    if(testArray.includes(removeElem[i])){
+        let index = testArray.indexOf(removeElem[i]);
+        testArray.splice(index, 1);
+    }
+   }     
+   return testArray;                 
 };
 
 // Do not edit below this line
