b3bcfbf77b6a8cbeca141d8f3c5f3ce1683729c1
Takafumi Mashiko <tmashiko7@gmail.com>
2023-04-20 19:30:21 -0400

    finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let convCelsius = (fahrenheit - 32)*(5/9);
+  convCelsius = Math.round(convCelsius * 10)/10;
+  return convCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let convFahrenheit = celsius*(9/5) + 32;
+  convFahrenheit = Math.round(convFahrenheit * 10)/10;
+  return convFahrenheit;
 };
 
 // Do not edit below this line
