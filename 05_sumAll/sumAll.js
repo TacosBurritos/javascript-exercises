6b8bc8e7c8a9a6b09db6e69ec47295fcbb19f254
Takafumi Mashiko <tmashiko7@gmail.com>
2023-04-20 18:32:32 -0400

    first try

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    for(start; start < end; start++){
+        sum += start;
+    }
+    return sum;
 };
 
 // Do not edit below this line
