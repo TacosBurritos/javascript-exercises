11a4551f189941ddece4670b3b3c77ca5453e890
Takafumi Mashiko <tmashiko7@gmail.com>
2023-05-19 21:36:01 -0400

    completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(anArray) {
+	const totalSum = anArray.reduce((accumulator, currentValue) => 
+    accumulator + currentValue, 0
+  );
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(anArray) {
+  const total = anArray.reduce((accumulator, currentValue) => 
+    accumulator * currentValue, 1
+  );
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a <= 1){
+    return 1;
+  }
+	return factorial(a-1)*a;
 };
 
 // Do not edit below this line
